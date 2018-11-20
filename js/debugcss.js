e50678a5b617c419a30f83ab42fe5249ecf647da
healthpotionstudios <43428482+healthpotionstudios@users.noreply.github.com>
2018-11-20 11:00:28 -0600

    Update debugcss.js

--- js/debugcss.js
+++ js/debugcss.js
@@ -247,7 +247,7 @@
 
     var outlinerContainer = document.createElement('div');
     outlinerContainer.className = 'boxvis' + (query.noln ? ' noln' : '') + (query.nobg ? ' nobg' : '');
-    //document.body.appendChild(outlinerContainer);
+    document.body.appendChild(outlinerContainer);
 
     outlinerContainer.innerHTML = html;
     var addedElements = outlinerContainer.childNodes;
